464373db637dee7fa90ee5335e54ce8434f88179
Richard <richardjonhshm@gmail.com>
2024-09-07 07:14:06 -0300

    Redirecionar para orcamento no login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,9 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->route("orcamento");
+
+        //return redirect()->intended(route('orcamento', absolute: false));
     }
 
     /**
