92326150a8950f3ae36510d6de7c349279e56975
Richard <richardjonhshm@gmail.com>
2024-09-12 16:13:52 -0300

    Subindo Estrela

--- resources/views/estrela/index.blade.php
+++ resources/views/estrela/index.blade.php
@@ -4,41 +4,39 @@
 
             {{--Coluna 01--}}
             <div class="flex justify-between items-center">
-                <div class="flex flex-1 justify-between">
+                <div class="flex justify-between">
 
-                    <div class="flex flex-col text-center text-white" style="font-size: 1.5em;line-height: 1em;font-style: italic;">
+                    <div class="flex flex-col text-center text-white" style="font-size: 1em;line-height: 1em;font-style: italic;">
                         <span class="font-italic">Ranking</span>
                         <span>de
                             <span style="color:#ffc107;">Vendas</span>
                         </span>
                     </div>
 
-                    <div>
-                        <img src="{{asset('hapvida-notreDame.png')}}" alt="Hapvida" style="width:200px;">
-                    </div>
 
 
 
 
                 </div>
 
-                <div class="flex flex-1 flex-col text-white text-center" style="line-height: 1.6em;">
-                    <span style="font-size:2em;font-weight:bold;">{{$ano_atual}}</span>
+                <div class="flex flex-col text-white text-center" style="line-height: 1.6em;">
+                    <span style="font-size:1em;font-weight:bold;">{{$ano_atual}}</span>
                     <span>{{$semestre}}º Semestre</span>
                 </div>
                 <div class="flex flex-col justify-end">
-                    <span class="text-white w-100 d-block font-italic text-center" style="font-size:1.2em;font-style: italic;">Programa Estrela</span>
-                    <div class="flex items-center justify-between" style="border:1px solid #FFF;border-radius:5px;margin-bottom:10px;line-height: 1.1em;">
-                        <svg class="w-10 h-10 text-gray-800 text-white hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+
+                    <div class="flex items-center" style="border:1px solid #FFF;border-radius:5px;margin-bottom:10px;line-height: 1.1em;align-content: flex-start;">
+                        <svg class="w-8 h-8 text-gray-800 text-white hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M11 9a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"/>
                             <path fill-rule="evenodd" d="M9.896 3.051a2.681 2.681 0 0 1 4.208 0c.147.186.38.282.615.255a2.681 2.681 0 0 1 2.976 2.975.681.681 0 0 0 .254.615 2.681 2.681 0 0 1 0 4.208.682.682 0 0 0-.254.615 2.681 2.681 0 0 1-2.976 2.976.681.681 0 0 0-.615.254 2.682 2.682 0 0 1-4.208 0 .681.681 0 0 0-.614-.255 2.681 2.681 0 0 1-2.976-2.975.681.681 0 0 0-.255-.615 2.681 2.681 0 0 1 0-4.208.681.681 0 0 0 .255-.615 2.681 2.681 0 0 1 2.976-2.975.681.681 0 0 0 .614-.255ZM12 6a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z" clip-rule="evenodd"/>
                             <path d="M5.395 15.055 4.07 19a1 1 0 0 0 1.264 1.267l1.95-.65 1.144 1.707A1 1 0 0 0 10.2 21.1l1.12-3.18a4.641 4.641 0 0 1-2.515-1.208 4.667 4.667 0 0 1-3.411-1.656Zm7.269 2.867 1.12 3.177a1 1 0 0 0 1.773.224l1.144-1.707 1.95.65A1 1 0 0 0 19.915 19l-1.32-3.93a4.667 4.667 0 0 1-3.4 1.642 4.643 4.643 0 0 1-2.53 1.21Z"/>
                         </svg>
                         <span class="flex flex-col flex-wrap text-white">
-                            <span style="color:#ffc107;margin-bottom:0;padding-bottom:0;">Melhores</span>
-                            <span style="display:flex;">VENDEDORES</span>
+                            <span style="color:#ffc107;margin-bottom:0;padding-bottom:0;" class="text-sm">Melhores</span>
+                            <span style="display:flex;" class="text-sm">VENDEDORES</span>
                         </span>
                     </div>
+
                 </div>
             </div>
             {{--Fim Coluna 01--}}
