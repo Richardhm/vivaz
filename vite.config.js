fadb463a6b387d894e2426e69c5ac34d397284f1
Richard <richardjonhshm@gmail.com>
2024-11-07 02:28:30 -0300

    Adicionando app.js para o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                //'resources/js/app.js',
-            ],
-            refresh: true,
+                'resources/js/app.js',
+            ]
         }),
     ],
 });
