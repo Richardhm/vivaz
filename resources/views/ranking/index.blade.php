f0547af64be2252f01a6b529c7b934ebb73bb64b
Richard <richardjonhshm@gmail.com>
2024-11-29 13:40:50 -0300

    Tirar o reverb do servidor continua sem som

--- resources/views/ranking/index.blade.php
+++ resources/views/ranking/index.blade.php
@@ -189,10 +189,10 @@
     </script>
 
     <script type="module">
-        Echo.channel('ranking-channel').listen('.ranking.updated', (event) => {
-            //desbloquearAudio()
-            verificarTrocaDeLider(event.novoRanking, event.venda);
-        });
+        // Echo.channel('ranking-channel').listen('.ranking.updated', (event) => {
+        //     //desbloquearAudio()
+        //     verificarTrocaDeLider(event.novoRanking, event.venda);
+        // });
     </script>
 
 
